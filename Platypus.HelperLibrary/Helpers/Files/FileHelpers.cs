1cf8dd14602fe1a69674a031f820fbb5a68d527e
Machacaz <jose.machacaz@outlook.com>
2023-11-07 00:43:13 +0000

    add new helper methods

--- Platypus.HelperLibrary/Helpers/Files/FileHelpers.cs
+++ Platypus.HelperLibrary/Helpers/Files/FileHelpers.cs
@@ -1,16 +1,41 @@
-﻿namespace Platypus.HelperLibrary.Helpers
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Platypus.HelperLibrary.Helpers;
+public static class FileHelper
 {
-    using Newtonsoft.Json;
-
-    public static class FileHelper
+    public static string ReadFileContent(params string[] filepath)
     {
-        public static string ReadFileContent(params string[] filepath)
-        {
-            var filePath = Path.Combine(filepath);
-            if (!File.Exists(filePath))
-                throw new FileNotFoundException($"File {filePath} not found");
+        var filePath = Path.Combine(filepath);
+        if (!File.Exists(filePath))
+            throw new FileNotFoundException($"File {filePath} not found");
 
-            return File.ReadAllText(filePath);
-        }
+        return File.ReadAllText(filePath);
+    }
+
+    //public static byte[] FileToByteArray(IFormFile formFile)
+    //{
+    //    using MemoryStream memoryStream = new MemoryStream();
+    //    formFile.CopyTo(memoryStream);
+    //    return memoryStream.ToArray();
+    //}
+
+    //public static Stream FileToStream(IFormFile formFile)
+    //{
+    //    MemoryStream ms = new MemoryStream();
+    //    formFile.CopyTo(ms);
+    //    return ms;
+    //}
+
+    public static string GenerateRandomFileNameByFileName(string fileName)
+    {
+        var fName = GenerateRandomFileNameByExtension(Path.GetExtension(fileName));
+
+        fName = string.Concat(Path.GetFileNameWithoutExtension(fileName), "_", fName);
+        return fName;
+    }
+
+    public static string GenerateRandomFileNameByExtension(string extension)
+    {
+        return String.Concat(Path.GetRandomFileName(), extension);
     }
 }
---
+++ Platypus.Tests/FileHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using Platypus.HelperLibrary.Helpers;
+
+namespace Platypus.Tests;
+
+public class FileHelperTests
+{
+    [Theory(DisplayName = "Random File Name - Extension")]
+    [InlineData(".jpg")]
+    public void RandomFileName_Extension(string value)
+    {
+        var output = FileHelper.GenerateRandomFileNameByExtension(value);
+
+        Assert.NotNull(output);
+    }
+
+    [Theory(DisplayName = "Random File Name - FileName")]
+    [InlineData("testeFile.docx")]
+    public void RandomFileName_FileName(string value)
+    {
+        var output = FileHelper.GenerateRandomFileNameByFileName(value);
+
+        Assert.NotNull(output); 
+    }
+}
